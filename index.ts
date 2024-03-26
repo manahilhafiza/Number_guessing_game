e8772259be71545874d3f2f8e3b0c8e7771658e4
manahilhafiza <161109728+manahilhafiza@users.noreply.github.com>
2024-03-26 16:36:41 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const randomNumber = 13;
+
+const answers = await inquirer.prompt([
+{
+   name: "userGuesNumber",
+   type: "number",
+   message: "please guess a number: ",
+},
+]);
+ if(answers.userGuesNumber === randomNumber){
+        console.log("congratulation! you guessed right number. ")
+ }else{
+    console.log("you guess wrong number")
+ }
+
